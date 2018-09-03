12d38d5db687c6025061a0ee45703c4cd26ef466
Anna Rusyayeva <annarusyayeva@MacBook-Pro-Rusyayeva.local>
2018-09-03 14:53:17 +0500

    end of 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule }                            from '@angular/platform-browser';
+import { ErrorHandler, NgModule }                   from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen }                             from '@ionic-native/splash-screen';
+import { StatusBar }                                from '@ionic-native/status-bar';
+import { NewPlacePage }                             from '../pages/new-place/new-place';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp }         from './app.component';
+import { HomePage }      from '../pages/home/home';
+import { PlacesService } from '../services/places.service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    NewPlacePage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +22,14 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    NewPlacePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    PlacesService
   ]
 })
 export class AppModule {}
